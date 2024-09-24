09a74c139cbb804cfdca90ea00cf0bda0bf6cf8e
Dourfyt <gamemastertv2016@gmail.com>
2024-09-24 10:04:44 +0300

    Created

--- tg.py
+++ tg.py
@@ -1,1 +1,26 @@
-import aiogram
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+import configparser
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+config = configparser.ConfigParser()
+config.read('config.ini')
+# Объект бота
+bot = Bot(token=config['BOT']["token"])
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
