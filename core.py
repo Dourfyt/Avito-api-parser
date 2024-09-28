07f22d5888c740769c9f360d9dccd116c124a540
Dourfyt <gamemastertv2016@gmail.com>
2024-09-29 00:57:58 +0300

    e

--- core.py
+++ core.py
@@ -84,8 +84,9 @@ class WBParse:
 
             for ticket in self.tickets_list:
                 if ticket not in ids:
-                    tickets.delete(ticket)
                     print(f"{ticket} удален в цикле")
+                    tickets.delete(ticket)
+
         except Exception as e:
             print(f"Ошибка при обработке: {e}")
 
