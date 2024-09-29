d7856165fccb42f45e6a6e78276ab0a7adfb49f9
Dourfyt <gamemastertv2016@gmail.com>
2024-09-29 14:19:25 +0300

    e

--- bot.py
+++ bot.py
@@ -5,11 +5,9 @@ from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters.command import Command
 import configparser
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.filters import Command, StateFilter
+from aiogram.filters import Command, StateFilter, BaseFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
-from multiprocessing import Process
-from core import main as run_sel
 
 
 
@@ -20,33 +18,39 @@ bot = Bot(token=config['BOT']["token"])
 dp = Dispatcher(storage=MemoryStorage())
 file = tg.ticket.File('tg/tickets')
 
+users = config["BOT"]["PERSON"].split(",")
+
+class IsAllowedUser(BaseFilter):
+    async def __call__(self, message: Message) -> bool:
+        return message.from_user.id in users
+
 class Tickets(StatesGroup):
     add_ticket = State()
 
-@dp.message(Command("start"))
+@dp.message(Command("start"), IsAllowedUser())
 async def cmd_start(message: types.Message, state: FSMContext):
     await message.answer("Здравствуйте, я бот для работы с заявками WB! Введите /help, для ознакомления с командами")
     await state.clear()
 
-@dp.message(Command("help"))
+@dp.message(Command("help"), IsAllowedUser())
 async def cmd_start(message: types.Message, state: FSMContext):
     await message.answer("Добавить поставку - /add номер поставки\nУдалить поставку - /del номер поставки\nПросмотреть файл - /show\nСбросить файл - /reset")
 
-@dp.message(Command("add"), F.text)
+@dp.message(Command("add"), IsAllowedUser())
 async def add_ticket(message: types.Message, state: FSMContext):
     file.add(message.text.split(' ')[-1])
     await message.answer("Успешно!")
 
-@dp.message(Command("del"))
+@dp.message(Command("del"), IsAllowedUser())
 async def del_ticket(message: types.Message, state: FSMContext):
     file.delete(message.text.split(' ')[-1])
     await message.answer("Успешно")
 
-@dp.message(Command('show'))
+@dp.message(Command('show'), IsAllowedUser())
 async def show(message: types.Message, state: FSMContext):
     await message.answer(file.show())
 
-@dp.message(Command('reset'))
+@dp.message(Command('reset'), IsAllowedUser())
 async def show(message: types.Message, state: FSMContext):
     await message.answer(file.rewrite())
 
