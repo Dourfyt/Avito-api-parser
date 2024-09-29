5e09c846379c01c3dae795ec22340cdfe31fe260
Dourfyt <gamemastertv2016@gmail.com>
2024-09-29 14:44:49 +0300

    e

--- core.py
+++ core.py
@@ -13,6 +13,8 @@ import configparser
 import json
 from tg.ticket import File
 
+global is_empty
+is_empty = False
 
 config = configparser.ConfigParser(interpolation=None)
 config.read("config.ini")
@@ -41,8 +43,6 @@ class WBParse:
     def __parse_page(self):
         """Парсит открытую страницу"""
         time.sleep(1)
-        global is_empty
-        is_empty = False
         try:
             # Читаем существующие ID из файла tickets.txt
             if os.path.isfile('tg/tickets.txt'):
