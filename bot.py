93041e7358976ac34e8f8fde2c01c26802e33ee0
Dourfyt <gamemastertv2016@gmail.com>
2024-09-29 12:28:53 +0300

    e

--- bot.py
+++ bot.py
@@ -1,55 +1,55 @@
 import asyncio
 import logging
-import configparser
-from aiogram import Bot, Dispatcher, types
+import tg.ticket
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters.command import Command
+import configparser
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
 from multiprocessing import Process
-import signal
-import os
-from core import main as run_sel  # Импорт функции main из core.py
+from core import main as run_sel
+
+
 
 logging.basicConfig(level=logging.INFO)
 config = configparser.ConfigParser()
 config.read('config.ini')
-bot = Bot(token=config['BOT']["TOKEN"])
+bot = Bot(token=config['BOT']["token"])
 dp = Dispatcher(storage=MemoryStorage())
+file = tg.ticket.File('tg/tickets')
 
-# Глобальная переменная для хранения процесса
-proc = None
+class Tickets(StatesGroup):
+    add_ticket = State()
 
-@dp.message(Command('start'))
+@dp.message(Command("start"))
 async def cmd_start(message: types.Message, state: FSMContext):
     await message.answer("Здравствуйте, я бот для работы с заявками WB! Введите /help, для ознакомления с командами")
     await state.clear()
 
-@dp.message(Command('help'))
-async def cmd_help(message: types.Message, state: FSMContext):
-    await message.answer("Добавить поставку - /add номер поставки\nУдалить поставку - /del номер поставки\nПросмотреть файл - /show\nЗапустить процесс - /run\nОстановить процесс - /stop")
+@dp.message(Command("help"))
+async def cmd_start(message: types.Message, state: FSMContext):
+    await message.answer("Добавить поставку - /add номер поставки\nУдалить поставку - /del номер поставки\nПросмотреть файл - /show")
 
-@dp.message(Command('run'))
-async def run(message: types.Message, state: FSMContext):
-    global proc
-    if proc and proc.is_alive():
-        await message.answer("Процесс уже запущен.")
-    else:
-        proc = Process(target=run_sel)
-        proc.start()
-        await message.answer("Процесс запущен.")
+@dp.message(Command("add"), F.text)
+async def add_ticket(message: types.Message, state: FSMContext):
+    file.add(message.text.split(' ')[-1])
+    await message.answer("Успешно!")
 
-@dp.message(Command('stop'))
-async def stop(message: types.Message, state: FSMContext):
-    global proc
-    if proc and proc.is_alive():
-        proc.terminate()
-        proc.join()
-        await message.answer("Процесс остановлен.")
-    else:
-        await message.answer("Процесс не запущен.")
+@dp.message(Command("del"))
+async def del_ticket(message: types.Message, state: FSMContext):
+    file.delete(message.text.split(' ')[-1])
+    await message.answer("Успешно")
+
+@dp.message(Command('show'))
+async def show(message: types.Message, state: FSMContext):
+    await message.answer(file.show())
+
 
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
